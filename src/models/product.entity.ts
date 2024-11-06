4093cd8a5286e8e12597d5e4cf0a1c082c11a89b
Azumi S <azumisaito@gmail.com>
2024-11-06 14:14:42 +0900

    Entityの記載

---
+++ src/models/product.entity.ts
@@ -0,0 +1,22 @@
+import e from 'express';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+// @Entity() 以下に書かれたクラスがRDBMのテーブルにマッピングされる
+@Entity()
+export class Product {
+  // @PrimaryGeneratedColumn() は主キーを自動生成するためのデコレータ。auto-incrementの値を生成する
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  price: number;
+}
