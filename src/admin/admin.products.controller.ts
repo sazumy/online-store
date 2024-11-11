d4a5e9cb1d52239e6ef927f37304046084965354
Azumi S <azumisaito@gmail.com>
2024-11-11 16:55:45 +0900

    updateメソッドの実装

--- src/admin/admin.products.controller.ts
+++ src/admin/admin.products.controller.ts
@@ -44,4 +44,32 @@ export class AdminProductsController {
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
   }
+
+  @Get('/:id')
+  @Render('admin/products/edit')
+  async edit(@Param('id') id: string) {
+    const viewData = [];
+    viewData['title'] = 'Admin Page - Admin - Online Store';
+    viewData['product'] = await this.productsService.findOne(Number(id));
+
+    return { viewData: viewData };
+  }
+
+  @Post('/:id/update')
+  @UseInterceptors(FileInterceptor('image', { dest: './public/uploads' }))
+  @Redirect('/admin/products')
+  async update(
+    @Body() body,
+    @UploadedFile() file: Express.Multer.File,
+    @Param('id') id: string,
+  ) {
+    const product = await this.productsService.findOne(Number(id));
+    product.setName(body.name);
+    product.setDescription(body.description);
+    product.setPrice(body.price);
+    if (file) {
+      product.setImage(file.filename);
+    }
+    await this.productsService.createOrUpdate(product);
+  }
 }
